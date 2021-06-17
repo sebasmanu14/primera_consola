ffcb4c0840461956d2ee148a88981f43108d4251
sebasmanu14 <sas.manobanda@yavirac.edu.ec>
2021-06-17 13:10:07 -0500

    tercer consola

---
+++ Nueva carpeta/tercerConsola/tercerConsola.consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tercerConsola.consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

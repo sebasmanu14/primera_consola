2713bc00685053cd3bde95ba52a8b48607ae40f9
sebasmanu14 <sas.manobanda@yavirac.edu.ec>
2021-06-17 12:51:41 -0500

    nuevo cambio

--- miPrimerConsola/miPrimerConsola.consola/Program.cs
+++ miPrimerConsola/miPrimerConsola.consola/Program.cs
@@ -6,7 +6,7 @@ namespace miPrimerConsola.consola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo!");
+            Console.WriteLine("Hola  Mundo!");
         }
     }
 }

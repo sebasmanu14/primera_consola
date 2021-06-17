5e60f02cb6aec536a087f44d9e0adc794b558230
sebasmanu14 <sas.manobanda@yavirac.edu.ec>
2021-06-17 12:44:09 -0500

    primer prueba

---
+++ miPrimerConsola/miPrimerConsola.consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace miPrimerConsola.consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}

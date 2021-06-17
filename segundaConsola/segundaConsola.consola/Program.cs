8f050ed883eaad4343a9fb161a38e23fab212a88
sebasmanu14 <sas.manobanda@yavirac.edu.ec>
2021-06-17 13:02:42 -0500

    cambio en master

---
+++ segundaConsola/segundaConsola.consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace segundaConsola.consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Profeee");
+        }
+    }
+}
